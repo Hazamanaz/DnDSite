a4531f13b7ac992b3bba9f4a73e41c882e39b463
Baz <harrysargent@live.co.uk>
2018-03-20 11:13:55 +0000

    Folder Cleanup
    Code unchanged

---
+++ sql/Create/init_db.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE databasename;
+CREATE TABLE table_name (
+    column1 datatype,
+    column2 datatype,
+    column3 datatype,
+   ....
+);
+CREATE TABLE table_name (
+    column1 datatype,
+    column2 datatype,
+    column3 datatype,
+   ....
+);
